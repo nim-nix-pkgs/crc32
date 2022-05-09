60c0d3c45594bb40dc645d32c2e5c337faa655a1
hugosenari <hugosenari@gmail.com>
2022-05-09 20:36:22 -0300

    chore: re index crc32

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."crc32-master".dir   = "master";
-  inputs."crc32-master".owner = "nim-nix-pkgs";
-  inputs."crc32-master".ref   = "master";
-  inputs."crc32-master".repo  = "crc32";
-  inputs."crc32-master".type  = "github";
-  inputs."crc32-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."crc32-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
